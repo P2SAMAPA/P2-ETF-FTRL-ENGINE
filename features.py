93fc92b02d0bcc0c3a8398b6d0fef8b1c172b55d
P2SAMAPA <pranabpattanaik@gmail.com>
2026-03-09 04:49:47 +0800

    Update features.py

--- features.py
+++ features.py
@@ -5,7 +5,7 @@
 import numpy as np
 import pandas as pd
 import sys, os
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
+sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
 import config as cfg
 
 
