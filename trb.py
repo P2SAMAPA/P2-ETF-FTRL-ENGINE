d3a71703623a995a6d4b11839e183ac8d3bdd19a
P2SAMAPA <pranabpattanaik@gmail.com>
2026-03-09 04:47:14 +0800

    Update trb.py

--- trb.py
+++ trb.py
@@ -7,7 +7,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import math
 import sys, os
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
+sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
 import config as cfg
 
 
