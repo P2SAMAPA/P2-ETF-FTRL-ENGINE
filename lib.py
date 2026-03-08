f53b9fbffa8ebe3786ac8e1dd66666dbfebf70f2
P2SAMAPA <pranabpattanaik@gmail.com>
2026-03-09 04:48:08 +0800

    Update lib.py

--- lib.py
+++ lib.py
@@ -8,7 +8,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import math
 import sys, os
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
+sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
 import config as cfg
 
 
